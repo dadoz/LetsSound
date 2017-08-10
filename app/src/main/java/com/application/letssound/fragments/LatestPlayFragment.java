1df3dd54c2653804cdc7f6902777fde076b6b7e8
dadoz <tunnus86@gmail.com>
2017-08-10 23:33:33 +0200

    added icons on searchList and historyList

--- app/src/main/java/com/application/letssound/fragments/LatestPlayFragment.java
+++ app/src/main/java/com/application/letssound/fragments/LatestPlayFragment.java
@@ -17,6 +17,6 @@ public class LatestPlayFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_settings, container, false);
+        return inflater.inflate(R.layout.fragment_latest_played_layout, container, false);
     }
 }
